2700003f3a9722023e6a69588776f7b703dc5d9b
RandomTripy <46785005+RandomTripy@users.noreply.github.com>
2020-02-14 11:39:17 +0900

    Create Calculater.py
    At First code. 
    
    I have a thanks to Nomad Coders.
    
    This is homeworks anyway :)

---
+++ Calculater.py
@@ -0,0 +1,35 @@
+a = input ("Enter the number if you want a plus or divide whatever = ")
+b = input ("Okay.. well... enter the other number please")
+
+m = int(a)
+n = int(b)
+
+
+def plus(a, b):
+  print(a + b)
+
+def minus(a, b):
+  print(a - b)
+
+def multiply(a, b):
+  print(a * b)
+
+def division(a, b):
+  print(a / b)
+
+def quotient(a, b):
+  print(a // b)
+
+def Remainder(a, b):
+  print(a % b)
+
+def nega(a):
+  print(-a)
+
+plus(m, n)
+minus(m, n)
+multiply(m, n)
+division(m, n)
+quotient(m, n)
+Remainder(m, n)
+nega(m)
